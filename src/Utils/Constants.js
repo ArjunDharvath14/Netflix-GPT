d8590dde5a76a29c9de3792d5fdcecbb373cfab7
Arjun Dharvath <arjundharvath14@gmail.com>
2025-10-02 12:39:11 +0530

    Update Constants.js

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,11 +1,5 @@
 export const LOGO="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const USER_AVATAR="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Virat_Kohli_portrait.jpg/1132px-Virat_Kohli_portrait.jpg?20180427075657";
-export const API_OPTIONS={
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOTljMGNmMWRhYTFkZTY2YjM3ZTM3NzI4YTI0ZDRkYSIsIm5iZiI6MTc1OTIyMzU0OC40OTIsInN1YiI6IjY4ZGI5ZWZjNzZiOGU5ZWI0ODUzMTJkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uzhJ1K-YNaWXUDMcbPGFBMxgu5rufR2L11qIu8VAJvk'
-  }
-};
+export const API_OPTIONS={#YOUR_TMDB_API_KEY};
 export const IMG_CDN_URL="https://image.tmdb.org/t/p/w500";
 export const BG_URL="https://assets.nflxext.com/ffe/siteui/vlv3/bebd95d0-65f9-41a9-9d12-4794db63653e/web/IN-en-20250922-TRIFECTA-perspective_5e75cfb4-3797-4f17-866b-181ff91a51dd_large.jpg";
